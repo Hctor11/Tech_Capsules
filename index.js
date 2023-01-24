6e0c7137c36c1182cafe898370f7e0ee9f66ed30
hector <hectorrrivera1010@gmail.com>
2023-01-23 22:46:12 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const app = express();
+const port = process.env.PORT || 3000
+
+// configurar handlebars
+app.engine('handlebars', expressHandlebars.engine({
+    defaultLayout: 'main',
+}))
+
+app.set('view engine', 'handlebars');
+
+// agregar nuestros directirio public a express
+app.use(express.static(__dirname + '/public'))
+
+//getters
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+// custon 404 page
+app.use((req, res) => {
+    res.status(404)
+    res.render('404')
+})
+
+app.listen(port, () => {
+    console.log(`App en el puerto ${port}`);
+})
